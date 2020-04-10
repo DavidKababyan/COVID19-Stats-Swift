065dbb22bc2b46659bb1f9793edc7c6a8c0e94e3
DavidFaraday <iosdevschool@gmail.com>
2020-04-10 14:42:28 +0300

    Initial Commit

---
+++ COVID Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID Data
+//
+//  Created by David Kababyan on 10/04/2020.
+//  Copyright © 2020 David Kababyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
